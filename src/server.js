ba61cc9262c562c7cdc5653e7f8480b1f39f309a
Mihai <mihaimihai.prut@gmail.com>
2022-06-16 09:14:36 +0200

    Created /fail path Create Node.js server

--- src/server.js
+++ src/server.js
@@ -26,6 +26,22 @@ app.get('/', (req, res) => {
   res.send(responseMessage);
 });
 
+app.get('/fail', (req, res) => {
+  // Get query parameters
+  const requestQuery = req.query;
+  const requestProbability = +requestQuery['probability'];
+
+  // Calculate the probability
+  let statusCode = 200;
+  if (Math.random() < (requestProbability / 100)) {
+    statusCode = 500;
+  }
+
+  // Sending the response
+  res.statusCode = statusCode;
+  res.send(`Status code is ${statusCode}`);
+});
+
 function getTimestamp() {
   const dateNow = new Date();
   const timestamp = dateNow.getTime();
